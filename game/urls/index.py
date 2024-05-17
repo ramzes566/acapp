244bdef4084cf1471b423882d9dcbd482f164c31
yuantong <1259520864@qq.com>
2024-05-18 03:50:49 +0800

    write html js views urls

---
+++ game/urls/index.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from game.views.index import index
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("playground/", include("game.urls.playground.index")),
+    path("menu/", include("game.urls.menu.index")),
+    path("settings/", include("game.urls.settings.index")),
+]
